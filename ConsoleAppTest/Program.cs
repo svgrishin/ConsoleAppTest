cf4e5a481856a0d87536a95d7cfdfdd0a9b138f4
Сергей Гришин <dr_grishin_91@mail.ru>
2023-06-08 13:10:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using Figures;
+
+Console.WriteLine("Введите стороны треугольника:");
+Console.WriteLine("Сторона 1: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Сторона 2: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Сторона 3: ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+List<Figure> figures = new List<Figure>();
+figures.Add(new Triangle(a, b, c));
+
+Figure triangle = new Triangle(a, b, c);
+
+Console.WriteLine($"Площать треугольника: {figures[0].GetS()}");
+
+Console.WriteLine(figures[0].GetDescription());
+
+
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
---
+++ Figures/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Figures
+{
+    public abstract class Figure
+    {
+        protected double S { get; set; }
+        protected double P { get; set; }
+        protected string description { get; set; }
+
+        public double GetS()
+        {
+            return S;
+        }
+        public string GetDescription()
+        {
+            return description;
+        }
+    }
+    public interface IFigure
+    {
+        protected void GetS();
+    }
+
+    public class Triangle : Figure, IFigure
+    {
+        private bool check;
+        double sideA, sideB, sideC;
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA > sideB)
+            {
+                this.sideA = sideB;
+                this.sideB = sideA;
+            }
+            else
+            {
+                this.sideA = sideA;
+                this.sideB = sideB;
+            }
+            if (this.sideB > sideC)
+            {
+                this.sideC = this.sideB;
+                this.sideB = sideC;
+            }
+            else
+            {
+                this.sideC = sideC;
+            }
+            check = CheckTriangle();
+            GetP();
+            GetS();
+        }
+
+        private void GetP()
+        {
+            P = (sideA + sideB + sideC) / 2;
+        }
+        public void GetS()
+        {
+            if (check)
+            {
+                S = Math.Sqrt(P * (P - sideA) * (P - sideB) * (P - sideC));
+            }
+            else
+            {
+                S = Math.Sqrt(P * (P - sideA) * (P - sideB));
+            }
+        }
+
+        private bool CheckTriangle()
+        {
+            double check = Math.Pow(sideC, 2) - Math.Pow(sideA, 2) - Math.Pow(sideB, 2);
+            if (check == 0)
+            {
+                description = "Прямоугольный треугольник";
+                return true;
+            }
+            else
+            {
+                description = "Непрямоугольный треугольник";
+                return false;
+            }
+        }
+    }
+    public class Circle : Figure, IFigure
+    {
+        int R;
+
+        public void GetS()
+        {
+            S = Math.PI * Math.Pow(R, 2);
+        }
+    }
+}
